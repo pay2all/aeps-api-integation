1c1c69694cd4eeb34ad7472a2a19058a2101f28c
sanjay yadav <sanjaydotlive@gmail.com>
2018-07-21 08:36:43 +0530

    Debit and credit url update
    Debit and credit url update with parameters

--- aeps_credit.php
+++ aeps_credit.php
@@ -1,5 +1,18 @@
 <?php
 
+//credit URL will be in POST method
+
+//below parameters will receive on every credit fucntion (example withdrawal)
+
+$agentcode = $_POST['agentcode'];
+$servicetype = $_POST['servicetype'];
+$amount = $_POST['amount'];
+$number = $_POST['number'];
+$orderid = $_POST['orderid'];
+$checksum = $_POST['checksum'];
+
+//Credit $amount to your agents 
+
 $data = [
      'RESP_CODE' => 300,
      'RESPONSE' => "SUCCESS",
--- aeps_debit.php
+++ aeps_debit.php
@@ -1,5 +1,15 @@
 <?php
 
+//get method [if agent money transfer or other service then this url will be execute]
+
+$agentcode=$_GET['agentcode'];
+$servicetype=$_GET['servicetype'];
+$amount=$_GET['amount'];
+$number=$_GET['number'];
+$orderid=$_GET['orderid'];
+
+//Debit $amount from your Agent
+
 $data = [
      'RESP_CODE' => 300,
      'RESPONSE' => "SUCCESS",
